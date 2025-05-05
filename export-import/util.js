91960c97945b51572bcb283c86df9e3577815201
nuhptr <nugrahaadi733@gmail.com>
2025-05-05 22:13:09 +0700

    feat: :rocket: export-import

---
+++ export-import/util.js
@@ -0,0 +1,3 @@
+export let apiKey = "AIzaSyBq3-u325k8683e40y6Y-qW740-j-3447s"
+export let apiUrl =
+    "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,status&id="
